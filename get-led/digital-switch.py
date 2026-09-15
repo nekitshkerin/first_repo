6a63c7377b0c2fe2d966fb5b4823ca31bac4c9b4
Nikita <nekit.shkerin@gmail.com>
2026-09-15 18:24:15 +0300

    Скрипты GPIO

---
+++ get-led/digital-switch.py
@@ -0,0 +1,18 @@
+import RPi.GPIO as GPIO
+import time 
+
+led = 26
+button = 13
+state = 0
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(led, GPIO.OUT)
+
+GPIO.setup(button, GPIO.IN)
+
+while True:
+    if GPIO.input(button):
+        state = not state
+        GPIO.output(led, state)
+        time.sleep(0.2)
+        print(state)
